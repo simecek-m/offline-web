0ca43cf4f1c25b46ed508fe6d74171fa9ee1c552
Martin Šimeček <simecek.m@seznam.cz>
2022-05-05 21:18:40 +0200

    add Tailwind CSS UI package

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="App">
-      <h1>offline</h1>
-      <p>Save tweets, threads or articles for later and then remove them without reading them. Who have time nowdays anyway.</p>
-    </div>
+    <>
+      <h1 className="text-3xl font-bold underline">offline</h1>
+      <p className="w-2/3 max-w-lg">Save tweets, threads or articles for later and then remove them without reading them. Who have time nowdays anyway.</p>
+    </>
   )
 }
