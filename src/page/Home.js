e4a6eb5bf1e7363dab0809a5d2eedaa775fcd455
Martin Šimeček <simecek.m@seznam.cz>
2022-05-05 20:41:21 +0200

    add first basic page

---
+++ src/page/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="App">
+      <h1>offline</h1>
+      <p>Save tweets, threads or articles for later and then remove them without reading them. Who have time nowdays anyway.</p>
+    </div>
+  )
+}
