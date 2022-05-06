2f4f28508d07d4284c9acc7e7a509f06c1a99b20
Martin Šimeček <simecek.m@seznam.cz>
2022-05-06 16:46:56 +0200

    implement Input component

---
+++ src/component/Input.js
@@ -0,0 +1,5 @@
+export default function Input({ placeholder = "", type="text", className }) {
+  return (
+    <input className={`bg-gray-100 px-5 py-4 rounded-md focus:outline-none ${className}`} type={type} placeholder={placeholder}/>
+  )
+}
