3939535868bc6c1dd9d6f6d99571aff5118343a1
Martin Šimeček <simecek.m@seznam.cz>
2022-05-06 16:30:25 +0200

    extend application theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand': '#FFF85A'
+      },
+      spacing: {
+        '120p': '120%',
+      }
+    },
   },
   plugins: [],
 }
