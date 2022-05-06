041c2a12f1ff202a2b1fd89d62c0013d11c2634a
Martin Šimeček <simecek.m@seznam.cz>
2022-05-06 19:29:43 +0200

    add hover effect for Button component

--- src/component/Button.js
+++ src/component/Button.js
@@ -2,7 +2,7 @@ import { ON_CLICK_DEFAULT } from "../constant";
 
 export default function Button({ text = "Button", onClick = ON_CLICK_DEFAULT, className }) {
   return (
-    <button className={`bg-black text-brand inline-block px-10 py-4 font-black rounded-md disabled:bg-gray-400 disabled:text-white ${className}`} onClick={onClick}>
+    <button className={`bg-black text-brand inline-block px-10 py-4 font-black rounded-md disabled:bg-gray-400 disabled:text-white transition-all hover:bg-brand hover:text-black ${className}`} onClick={onClick}>
       {text}
     </button>
   )
