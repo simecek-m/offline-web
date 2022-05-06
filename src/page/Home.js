a569822a4f46be28cf57dc5433a1703866880d4a
Martin Šimeček <simecek.m@seznam.cz>
2022-05-06 16:47:05 +0200

    update Home page

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,8 +1,14 @@
+import Button from "../component/Button";
+import Input from "../component/Input";
+import Title from "../component/Title";
+
 export default function Home() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">offline</h1>
-      <p className="w-2/3 max-w-lg">Save tweets, threads or articles for later and then remove them without reading them. Who have time nowdays anyway.</p>
-    </>
+    <div className="w-screen h-screen p-20">
+      <Title text="offline"/>
+      <p className="w-2/3 max-w-lg ">Save tweets, threads or articles for later and then remove them without reading them. Who have time nowdays anyway.</p>
+      <Input placeholder="URL" className="mr-1 w-96" />
+      <Button text="unwrap" />
+    </div>
   )
 }
