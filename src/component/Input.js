82654447890e34ed880c2be9bec4e70c6868f506
Martin Šimeček <simecek.m@seznam.cz>
2022-05-06 20:13:22 +0200

    get url address from input

--- src/component/Input.js
+++ src/component/Input.js
@@ -1,5 +1,5 @@
-export default function Input({ placeholder = "", type="text", className }) {
+export default function Input({ placeholder = "", type="text", className, ...rest }) {
   return (
-    <input className={`bg-gray-100 px-5 py-4 rounded-md focus:outline-none ${className}`} type={type} placeholder={placeholder}/>
+    <input className={`bg-gray-100 px-5 py-4 rounded-md focus:outline-none ${className}`} type={type} placeholder={placeholder} {...rest}/>
   )
 }
--- src/page/Home.js
+++ src/page/Home.js
@@ -3,15 +3,21 @@ import Input from "../component/Input";
 import Subtitle from "../component/Subtitle";
 import Title from "../component/Title";
 import { CgLink } from "react-icons/cg";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function Home() {
+  const [ url, setUrl ] = useState("https://");
+  const sendRequest = () => {
+    toast.success("Saved")
+  }
   return (
     <div className="w-screen h-screen p-20">
       <Title text="offline"/>
       <p className="w-2/3 max-w-lg ">Save tweets, threads or articles for later and then remove them without reading them. Who have time nowdays anyway.</p>
       <Subtitle icon={<CgLink />} text="Link" className="mt-10"/>
-      <Input placeholder="URL" className="mr-1 w-96 mt-5" />
-      <Button text="unwrap" />
+      <Input placeholder="URL" className="mr-1 w-96 mt-5" value={url} onChange={e => setUrl(e.target.value)}/>
+      <Button text="unwrap" onClick={sendRequest} />
     </div>
   )
 }
